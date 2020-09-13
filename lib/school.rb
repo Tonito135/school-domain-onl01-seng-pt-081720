a3e1335bc4a658928a402fc461e47ffde2b7c6d9
Anthony Hernandez <anthonyh2020@gmail.com>
2020-09-13 00:44:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,14 +3,7 @@ class School
   attr_accessor :student, :grade
   attr_reader :roster
   
-  ROSTERS = []
-  
-  def initialize(roster)
-    @roster = roster
-    ROSTERS << roster
-    ROSTERS.uniq!
-  end
-  
+ 
   
   
   
